5a98a6761d7f3dd0571e701aeac05179c9877ccc
margb7 <mariogonzalezgoian@gmail.com>
2022-04-28 19:57:22 +0200

    Limpiar main

--- Calendario/src/calendario/Calendario.java
+++ Calendario/src/calendario/Calendario.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package calendario;
 
 /**
  *
- * @author mariogb
  */
 public class Calendario {
 
@@ -14,7 +9,9 @@ public class Calendario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
     }
     
 }
