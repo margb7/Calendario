b7bafc1d2970addd2adc87d1c99b77c914fafa45
margb7 <mariogonzalezgoian@gmail.com>
2022-05-08 21:39:41 +0200

    Comprobacioón de nombre de usuario

--- Calendario/src/utilidades/Funciones.java
+++ Calendario/src/utilidades/Funciones.java
@@ -38,4 +38,28 @@ public class Funciones {
         return out;
     }
 
+    /**
+     * Comproba que o nome é sintácticamente válido para evitar rexistrar nomes con espacios por exemplo.
+     * Non comproba que o usuario esté xa rexistrado na base de datos.
+     * @param str a cadea co nome de usuario.
+     * @return true se é válido. Un nome válido contén como mínimo 3 caracteres e ningún espacio.
+     */
+    public static boolean nomeUsuarioValido(String str ) {
+
+        boolean out = false;
+
+        if(str.length() >= 3 ) {
+
+            if(!str.contains(" ") ) {
+
+                out = true;
+
+            }
+
+        } 
+
+        return out;
+
+    }
+
 }
